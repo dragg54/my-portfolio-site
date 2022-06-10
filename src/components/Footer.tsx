c9523919925542becec5687a7ae5f07ae0e7a449
dragg54 <ajibolasadiq@yahoo.com>
2022-06-10 15:40:06 +0100

    increased margin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FaFacebookF, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div className='h-10 w-full bg-gray-400 px-8 flex text-[#E34234] text-sm justify-end items-center font-[poppins]'>
+    <div className='h-10 w-full bg-gray-400 px-10 flex text-[#E34234] text-sm justify-end items-center font-[poppins]'>
           <FaFacebookF className='mr-2'/>
         <FaTwitter className='mr-2'/>
         <FaInstagram className='mr-2'/>
