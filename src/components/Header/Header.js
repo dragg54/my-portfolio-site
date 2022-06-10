6a0a91f5ed85d53de41b08bacada85569e6d8c2f
dragg54 <ajibolasadiq@yahoo.com>
2022-06-10 14:50:58 +0100

    changed text color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Nav from './Nav'
 import { FiMenu } from 'react-icons/fi'
+
 const Header = () => {
   return (
-    <div className='mb-16 flex w-full justify-between'>
+    <div className='mb-16 flex w-full justify-between animate-[fadeIn_linear_1s_infinite]'>
         <Nav />
         <div className='text-[#E34234] text-[1.5rem]'><FiMenu /></div>
         </div>
